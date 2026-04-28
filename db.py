8d1cf1cf1c2390424209ac88a7188372970f12b2
nirgunav <nirgunavalvekar@gmail.com>
2026-04-28 13:57:49 +0530

    Saved code

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+from psycopg2.extras import RealDictCursor
 
 DATABASE_URL = "postgresql://url_db_2yjn_user:0yiwgAX9ebtpf9bXY9XlCGElKoKQbwIy@dpg-d7n110hkh4rs73auv8cg-a.oregon-postgres.render.com/url_db_2yjn"
 
@@ -8,10 +9,6 @@ def get_db():
     db_url = os.getenv("url-db")
 
     if db_url:
-        return psycopg2.connect(DATABASE_URL)
-    return psycopg2.connect(
-        host="dpg-d7n110hkh4rs73auv8cg-a",
-        database="url-db",
-        user="url_db_2yjn_user",
-        password="0yiwgAX9ebtpf9bXY9XlCGElKoKQbwIy",
-    )
+        return psycopg2.connect(
+            os.getenv("DATABASE_URL"), cursor_factory=RealDictCursor
+        )
