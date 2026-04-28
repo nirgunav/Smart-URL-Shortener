e3493e3956d9b40e061ca4ac4985eeac8ba895a0
nirgunav <nirgunavalvekar@gmail.com>
2026-04-28 14:06:44 +0530

    Saved code for deployment

--- db.py
+++ db.py
@@ -2,8 +2,6 @@ import psycopg2
 import os
 from psycopg2.extras import RealDictCursor
 
-DATABASE_URL = "postgresql://url_db_2yjn_user:0yiwgAX9ebtpf9bXY9XlCGElKoKQbwIy@dpg-d7n110hkh4rs73auv8cg-a.oregon-postgres.render.com/url_db_2yjn"
-
 
 def get_db():
     db_url = os.getenv("DATABASE_URL")
